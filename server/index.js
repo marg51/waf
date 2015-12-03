ff8bfb131e9293a6adf5748717a6cea33a5e1706
Laurent Margirier <laurent.margirier@gmail.com>
2015-12-03 22:25:34 +0000

    feat — github login

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
-// require('./auth')
+require('./auth')
 
 var fs = require('fs')
+var config = require('./config')
 
 global._ = require('lodash')
 
@@ -8,8 +9,17 @@ import {store} from './store'
 
 var state = JSON.parse(fs.readFileSync('./state.json'))
 
-var io = require('socket.io')(4042);
 
+var io = require('socket.io')(4042)
+var socketioJwt = require('socketio-jwt')
+
+io.use(socketioJwt.authorize({
+  secret: config.secret,
+  handshake: true
+}));
+
+
+console.log("Socket listening on", 4042)
 
 store.dispatch({type:'//init/state', state})
 
@@ -18,6 +28,8 @@ store.subscribe(_.debounce(() => {
 }, 500))
 
 io.on('connection', function (socket) {
+  console.log('hello! ', socket.decoded_token);
+
   socket.on('state', function (data) {
     socket.emit('state', _.pick(store.getState(), ["stories", "columns", "board"]));
   });
@@ -28,6 +40,7 @@ io.on('connection', function (socket) {
 
     callback()
     action.from_network = true
+    action.from = _.pick(socket.decoded_token, ['name','avatar_url'])
     socket.broadcast.emit('dispatch', action)
   })
 });
---
+++ server/user_reducer.js
@@ -0,0 +1,42 @@
+export const INIT_STATE = {
+    items: {}
+}
+
+export function checkUser(state, id) {
+    if(!state.items[id]) {
+        throw new Error('no such column #'+id)
+    }
+}
+
+export function reducer(state = INIT_STATE, action) {
+    switch(action.type) {
+        case '//init/state':
+            return action.state.users || {}
+        case 'USER:ADD':
+
+            return _.merge({}, state, {
+                items: {
+                    [action.id]: {
+                        id: action.id,
+                        data: {
+                            token_id: action.id,
+                        },
+                        token: action.token
+                    }
+                }
+            })
+        case 'USER:UPDATE':
+            checkUser(state, action.id)
+
+            return _.merge({}, state, {
+                items: {
+                    [action.id]: {
+                        data: action.object
+                    }
+                }
+            })
+
+    }
+
+    return state
+}
