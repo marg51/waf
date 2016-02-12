0c62754b222827d432146f8e8f52afda7a045e9a
Laurent Margirier <laurent.margirier@gmail.com>
2016-02-12 18:27:37 +0000

    fix(plugin/github): remove test code

--- server/modules/github/index.js
+++ server/modules/github/index.js
@@ -9,13 +9,6 @@ const decorate = (action) => {
 
 export const GithubMiddleware = store => {
 
-    setTimeout(function() {
-        // this toggles waf's third task
-        console.log(store.getState().tasks.items["todo_f3d561fe-0bb0-4bf3-a548-5745e0d51154"].checked)
-        store.dispatch({type: 'TODO:UPDATE', id: "todo_f3d561fe-0bb0-4bf3-a548-5745e0d51154", todo: {checked: !store.getState().tasks.items["todo_f3d561fe-0bb0-4bf3-a548-5745e0d51154"].checked}, _metadata: {origin: 'github'}})
-        console.log(store.getState().tasks.items["todo_f3d561fe-0bb0-4bf3-a548-5745e0d51154"].checked)
-    }, 5000)
-
     return next => action => {
         if(action._metadata.origin !== "webapp") return next(action)
 
