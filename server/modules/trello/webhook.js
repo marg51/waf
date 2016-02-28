5664d32a637fe658f45f05f775444fcfe437ed63
Laurent Margirier <laurent.margirier@gmail.com>
2016-02-28 17:15:49 +0000

    feat(trello): implement more webhook actions

--- server/modules/trello/webhook.js
+++ server/modules/trello/webhook.js
@@ -9,9 +9,20 @@ server.use(restify.queryParser());
 server.use(restify.bodyParser());
 
 import * as storyActions from '../../../src/components/story/actions'
+import * as columnActions from '../../../src/components/column/actions'
 
 const decorate = (action) => {
     return _.set(action, '_metadata.origin', 'trello')
+}
+
+const getStoryId = function(state, trelloId) {
+    return _.filter(state.stories.items, (story) => _.get(story, '_metadata.trello.id') == trelloId)[0].id
+}
+const getColumnIdByStoryId = function(state, storyId) {
+    return _.filter(state.columns.items, (column) => column.stories.indexOf(storyId) >=0)[0].id
+}
+const getColumnId = function(state, trelloId) {
+    return _.filter(state.columns.items, (story) => _.get(story, '_metadata.trello.id') == trelloId)[0].id
 }
 
 export const TrelloWebhookMiddleware =  (store) => {
@@ -32,16 +43,39 @@ export const TrelloWebhookMiddleware =  (store) => {
                 case 'updateCard':
                     if(action.data.old.name) {
                         console.log(action.data.card.id)
-                        const id = _.filter(state.stories.items, (story) => _.get(story, '_metadata.trello.id') == action.data.card.id)[0].id
+                        const id = getStoryId(state, action.data.card.id)
                         console.log(storyActions.update({id, story: {title: action.data.card.name}}))
                         store.dispatch(decorate(storyActions.update({id, story: {title: action.data.card.name}})))
                     }
                     else if(action.data.old.desc) {
                         console.log(action.data.card.id)
-                        const id = _.filter(state.stories.items, (story) => _.get(story, '_metadata.trello.id') == action.data.card.id)[0].id
+                        const id = getStoryId(state, action.data.card.id)
                         console.log(storyActions.update({id, story: {description: action.data.card.desc}}))
                         store.dispatch(decorate(storyActions.update({id, story: {description: action.data.card.desc}})))
                     }
+                    // move position
+                    else if(action.data.old.pos) {
+                        var storyId = getStoryId(state, action.data.card.id)
+                        var id = getColumnIdByStoryId(state, storyId)
+                        store.dispatch(decorate(columnActions.moveStory({id, storyId, index: action.data.card.pos})))
+                    }
+                    // move column
+                    else if(action.data.old.idList) {
+                        var storyId = getStoryId(state, action.data.card.id)
+                        var oldColumnId = getColumnIdByStoryId(state, storyId)
+                        var id = getColumnId(state, action.data.card.idList)
+                        store.dispatch(decorate(columnActions.removeStory({id:oldColumnId, storyId})))
+                        store.dispatch(decorate(columnActions.addStory({id, storyId, index: 0})))
+                    }
+                    break
+                case 'addLabelToCard':
+                    var id = getStoryId(state, action.data.card.id)
+                    store.dispatch(decorate(storyActions.update({id, story: {labels: {[action.data.value]:true}}})))
+                    break;
+                case 'removeLabelFromCard':
+                    var id = getStoryId(state, action.data.card.id)
+                    store.dispatch(decorate(storyActions.update({id, story: {labels: {[action.data.value]:false}}})))
+                    break;
 
             }
         } catch(e) {
@@ -58,7 +92,3 @@ export const TrelloWebhookMiddleware =  (store) => {
 
     return next => action => next(action)
 }
-
-
-
-
